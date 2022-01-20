ddb92f0f1e43e7b6b5d182c81328f84ee6255698
chriskmil <93924532+chriskmil@users.noreply.github.com>
2022-01-19 20:18:19 -0500

    rewind challenge

--- js/site.js
+++ js/site.js
@@ -1,12 +1,31 @@
-// display a message to the user
-function getMessage() {
-    let userMessage = document.getElementById("message").value;
-    Swal.fire(
-        'button clicked ',
-        userMessage,
-        'success'
-    );
+//pull value from the page
+function getValues() {
+    let userString = document.getElementById("revString").value;
+
+    //call reverse a string
+    let revString = reverseAString(userString);
+
+    //display the reversed string
+    displayData(revString);
+}
+
+//this is our generate data function
+function reverseAString(userString) {
+
+    let revString = "";
+
+    for (let index = userString.length - 1; index >= 0; index--) {
+        revString += userString[index];
+    }
+
+    return revString;
+}
 
 
-    console.log(userMessage);
+//display out reversed strinf to the page
+function displayData(revString) {
+
+    document.getElementById("msg").innerHTML = `Your String reversed is: ${revString}`;
+    document.getElementById("alert").classList.remove('d-none');
+
 }
